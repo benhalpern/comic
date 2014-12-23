c8925b45c8d4848c7388b1413f7b706994683fe3
Ben Halpern <bendhalpern@gmail.com>
2014-12-22 20:09:52 -0500

    fade in bg-holder

--- js/application.js
+++ js/application.js
@@ -744,6 +744,7 @@ function loadCollections(){
     $.each(data, function( index, value ) {
       $("#collections").append('<option value="'+ value.id +'">'+ value.name +'</option>')
     });
+    $(".bg-holder").animate({"opacity":"1"},1300)
     loadCharacters(data[0]["id"])
     loadBackgrounds(data[0]["id"])
   })
